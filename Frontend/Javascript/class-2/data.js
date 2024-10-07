506fad8309bcc98bea990898a91d6ea3804e8c46
suvendu sahoo <sahoosuvendu516@gmail.com>
2024-10-07 12:03:41 +0530

    new

--- Frontend/Javascript/class-2/data.js
+++ Frontend/Javascript/class-2/data.js
@@ -11,7 +11,7 @@ let age= 23;
 let rollno=1541;
 console.log(typeof num1);*/
 //addition
-let num2=100;
+/*let num2=100;
 let num3=125;
 let addition=num2+num3;
 console.log(addition);
@@ -24,3 +24,29 @@ console.log(multiplication);
 //division
 let division=num3/num2;
 console.log(division);
+//NaN(not a num)
+let num4=0;
+let num5=0
+console.log(num4/num5);
+let num6=0;
+let num7=10;
+console.log(num6/num7);
+
+//Boolean
+let isMarried=false;
+let isSingle=true;
+console.log(typeof isMarried);
+console.log(typeof isSingle);
+console.log(isMarried);
+console.log(isSingle);*/
+
+//string
+let name = "SUVENDU";
+let city = "DHENKANAL";
+let rollno = 101;
+let age = 23;
+let gender = "Male";
+let pincode = 759015;
+let college = "USBM";
+let batch = 2025;
+console.log(`Name of the student is : ${name} ,City is : ${city},Rollno is : ${rollno},Age is : ${age},Gender is : ${gender},Pincode is : ${pincode},College name is : ${college},Batch is : ${batch}`);
