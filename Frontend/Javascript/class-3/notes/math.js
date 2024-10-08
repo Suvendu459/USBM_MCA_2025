5422ac0ff5a986c9c6572570c13c959739cb56d8
suvendu sahoo <sahoosuvendu516@gmail.com>
2024-10-08 11:54:16 +0530

    complete it

---
+++ Frontend/Javascript/class-3/notes/math.js
@@ -0,0 +1,57 @@
+//Mathematical operator
+/*let num1=5;
+let num2=3;
+let result= num1+num2;//addition operator
+//let result= num1-num2;//subtraction
+//let result= num1*num2;//multiplication
+//let result= num1/num2;//division
+console.log(result);
+//modulo operator:
+// to find out remainder
+let num3=17;
+let odd=(num3%2);
+console.log(odd);
+//Exponential operator:
+let square = 2**2;
+console.log(square);
+//relational operator
+//to compatre two values
+console.log(10>5);
+console.log(10<5);
+console.log(5>5);
+console.log(5>=5);
+console.log(10>=5);
+//Equality operator
+//`==` : value and data type check
+console.log(5==5);//boolean
+console.log(6==6);
+console.log(5=="5");
+console.log(5==="5");
+console.log(5!="5");*/
+
+//conditional statements
+//if(condition){
+// true code to be executed here
+//}
+
+//false value :
+// false,0,-0,"",``,'',null,undefined,NaN
+
+if (-0.0512){
+    console.log("it is true")
+
+}
+
+//task:
+//given totalBill,discountStartPrice if you satisfy the condition print discount
+//available otherwise print no discount
+
+let totalBill = 1000;
+let discountStartPrice = 300;
+if(totalBill>discountStartPrice){
+    console.log("Discount")
+}else{
+    console.log("No Discount")
+}
+//check if (male) and then check (22+) then he can able to marry
+
